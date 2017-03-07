e6c40da2098488964dee8bdc6952c058a2ce102e
canry41341 <canry334@yahoo.com>
2017-03-07 23:25:39 +0800

    lab1_B

---
+++ A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+cin>>n;
+repeat:
+cout<<n<<" ";
+		if(n == 1){ 
+			return 1;
+		}
+			else if(n%2 == 1){
+				n = 3*n + 1;
+			}
+			else{
+				n = n/2;
+			}
+goto repeat;
+}
